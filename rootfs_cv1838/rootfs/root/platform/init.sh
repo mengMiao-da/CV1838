d9ac36a7b925e53fc86f39090cdb4a3f4f5afa4a
cxgit <1217200430@qq.com>
2023-01-31 14:36:21 +0800

    rootfs  源码

---
+++ rootfs_cv1838/rootfs/root/platform/init.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+PLATFORM=/root/platform
+export PATH=${PATH}:${PLATFORM}
+
+a_rootfs=`cat /proc/cmdline | awk -F ' ' '{print $2}' | awk -F '=' '{print $2}'`
+
+ubiattach /dev/ubi_ctrl -m 8
+if [ $? != 0  ]; then
+	ubiformat -y /dev/mtd8
+	ubiattach /dev/ubi_ctrl -m 8
+	ubimkvol /dev/ubi1 -N CFG -m
+fi
+
+if [ ! -c /dev/ubi1_0 ]; then
+	mdev -s
+fi
+mount -t ubifs -o sync /dev/ubi1_0 /root/configs
+if [ $? != 0  ]; then
+	echo 'Mount /root/configs failed, Try formatting and remounting'
+	ubimkvol /dev/ubi1 -N CFG -m
+	mount -t ubifs -o sync /dev/ubi1_0  /root/configs
+fi
+
+if [ ${a_rootfs} != "ROOTFS2" ]; then
+	echo "/dev/mtd3" > /tmp/eapil_app_mtd
+else
+	echo "/dev/mtd6" > /tmp/eapil_app_mtd
+fi
+
+echo ${a_rootfs}
+if [ ${a_rootfs} != "ROOTFS2" ]; then
+	mount -t squashfs /dev/mtdblock3 /root/firmware
+else
+	mount -t squashfs /dev/mtdblock6 /root/firmware
+fi
+
+sleep 2
+
+mount /dev/mmcblk0p1 /mnt/sdcard -o errors=continue
+mount -o remount,rw /mnt/sdcard
+if [ -f /mnt/sdcard/rom.bin ]; then
+${PLATFORM}/ota_update 0 /mnt/sdcard/rom.bin
+fi
+
+firmware_init.sh
