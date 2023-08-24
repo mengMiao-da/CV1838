ca5770fefeb5c76642a277faef220cdde76c1ebf
cxgit <1217200430@qq.com>
2023-08-24 16:24:05 +0800

    新增安全的rootfs

---
+++ cv180x_cv181x_rootfs_anquan/cvitek_180x_nor/rootfs/root/platform/init.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+PLATFORM=/root/platform
+export PATH=${PATH}:${PLATFORM}
+mount -t jffs2 /dev/mtdblock6 /root/configs
+
+if [ -f /root/configs/updating ]; then
+        echo "activate plan b"
+	mount -t squashfs /dev/mtdblock5 /root/firmware
+else
+	mount -t squashfs /dev/mtdblock4 /root/firmware
+fi
+mount /dev/mmcblk0p1 /mnt/sdcard -o errors=continue
+mount -o remount,rw /mnt/sdcard
+if [ -f /mnt/sdcard/rom.bin ]; then
+${PLATFORM}/ota_update 1 /mnt/sdcard/rom.bin
+fi
+
+firmware_init.sh
